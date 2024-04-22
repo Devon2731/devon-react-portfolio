d254d58e83f26593d6122b8029862a684b879482
Devon2731 <devon.whitaker27@gmail.com>
2024-04-22 15:31:34 -0400

    implemented a new document into the resume file and also added a new animation for the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import Navigation from "./Navigation";
 import { Container, Row, Col } from "react-bootstrap";
@@ -6,12 +6,20 @@ import "./styles/header.css";
 
 // Header component
 function Header() {
+  const [isVisible, setIsInvisible] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() =>
+    setIsInvisible(true), 3000);
+    return () => clearTimeout(timeout)
+    }, []);
+
   return (
     <>
       <Container fluid className="bg-dark">
         <Row className="justify-content-center text-center mb-0">
           <Col className="pb-0">
-            <h1 className="text-white" style={{ fontSize: "70px" }}>
+            <h1 className={`text-white ${!isVisible ? "fade-in" : ""}`} style={{ fontSize: "70px" }}>
               <Link
                 to="React-Portfolio/"
                 className="text-white text-decoration-none"
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import "./styles/resume.css";
-import PDF from "../Document/DevoWhitakerATSfriendlyresumeDA.pdf"
+import PDF from "../Document/DevonWhitakerFFSAResume.pdf";
 
 // Resume component
 function Resume() {
